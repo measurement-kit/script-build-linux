6f56ab1e392bb894fad62e3e6bdcadb5fe18e79e
Simone Basso <bassosimone@gmail.com>
2019-06-16 17:05:42 +0200

    Repair MK v0.10.4 build
    1. when investigating
    https://github.com/measurement-kit/script-build-linux/issues/6, I did
    notice that a fix was to add the `-static` packages
    
    2. while working on it, I understood that such packages are
    probably what we need also to avoid compiling cURL and libmaxminddb
    manually and we could just use Alpine for that
    
    3. there are no libressl/openssl dangers here as Alpine has
    switched back to openssl as the default SSL library a while
    ago and all packages depend on it [1]
    
    I will now check whether we can build ooni/probe-cli again with
    this freshly pushed Docker image. If so, I'll open a PR.
    
    .. [1] https://twitter.com/jedisct1/status/961951492159758337

--- curl/download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-url=https://github.com/curl/curl/archive/curl-7_65_0.tar.gz
-sha256=78459796393004fea0f1f1a206d813aabd7393708052426356d5c639fd6bda06
--- libmaxminddb/download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-url=https://github.com/maxmind/libmaxminddb/archive/1.3.2.tar.gz
-sha256=c0785521c7e5515f1169db90ed6e51bc2a5a000377d0fbad87e4d5a791a6e364
